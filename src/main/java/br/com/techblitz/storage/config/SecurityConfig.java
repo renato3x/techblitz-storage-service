1a86ef05484be43dfc3a2ce3c426873401557deb
renato3x <pereirarenato21@gmail.com>
2025-05-04 23:56:43 -0300

    fix: remove unused imports

--- src/main/java/br/com/techblitz/storage/config/SecurityConfig.java
+++ src/main/java/br/com/techblitz/storage/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package br.com.techblitz.storage.config;
 
 import br.com.techblitz.storage.security.JwtCookieCleanerFilter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
